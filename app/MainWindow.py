5d727f847e84617a127ac8a74b54cf5a18e415bd
Andrexus23 <andrewsamsunguser@gmail.com>
2023-11-17 21:29:11 +0300

    efqwf

--- app/MainWindow.py
+++ app/MainWindow.py
@@ -1,25 +1,34 @@
 from enum import Enum
+from typing import Optional
 
 from PyQt5.QtWidgets import QMainWindow
 
-class Alogithm(Enum):
 
+class Alogithm(Enum):
+    """Энам."""
     HALF_DIVIDE = 'Метод половинного деления'
     GOLD_SECTION = 'Метод золотого сечения'
     MID_POINT = 'Метод средней точки'
+    NEWTON_RAPFSON = 'Метод Ньютона-Рафсона'
+    CHORDS = 'Метод хорд'
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.epsilon: Optional[float] = None
+        self.sigma: Optional[float] = None
+        self.a: Optional[float] = None
+        self.b: Optional[float] = None
+        self.func = None
 
     def initUi(self, ui):
         self.ui = ui
         self.ui.setupUi(self)
         self.setFixedSize(self.size())
-
+        self.ui.doubleSpinBoxEps.valueChanged.connect(self.updateEps)
         self.show()
 
-    def updateParams(self):
-        self.algo
-
-
+    def updateEps(self):
+        self.epsilon = self.ui.doubleSpinBoxEps.value()
+        print(self.epsilon)
