1ad8357bc67eb9bc9858dd6ebc075160e148cb27
Andrexus23 <andrewsamsunguser@gmail.com>
2023-11-17 21:07:56 +0300

    efnwef

--- app/MainWindow.py
+++ app/MainWindow.py
@@ -1,5 +1,12 @@
+from enum import Enum
+
 from PyQt5.QtWidgets import QMainWindow
 
+class Alogithm(Enum):
+
+    HALF_DIVIDE = 'Метод половинного деления'
+    GOLD_SECTION = 'Метод золотого сечения'
+    MID_POINT = 'Метод средней точки'
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,4 +15,11 @@ class MainWindow(QMainWindow):
     def initUi(self, ui):
         self.ui = ui
         self.ui.setupUi(self)
+        self.setFixedSize(self.size())
+
         self.show()
+
+    def updateParams(self):
+        self.algo
+
+
